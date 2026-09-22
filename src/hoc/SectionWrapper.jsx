570aedf820154fa7368cffb731a3ac9393f0bac1
Zaid <zaid.ahmed@ucalgary.ca>
2026-09-21 23:54:54 -0400

    fixed span issue.

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion, stagger } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { staggerContainer } from '../utils/motion';
 
@@ -7,12 +7,12 @@ const SectionWrapper = (Component, idName) =>
 function HOC() {
     return(
         <motion.section
+        id={idName || undefined}
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
         viewport={{ once: true, amount: 0.25 }}
-        className={`${styles.padding}, max-w-7xl mx-auto relative z-0`}>
-            <span className='hash-span' id={idName}>&nbsp;</span>
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0 scroll-mt-24`}>
             <Component />
         </motion.section>
     )
